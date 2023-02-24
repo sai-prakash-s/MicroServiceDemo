425e6d269d936f649298bee958916a8a5809ff91
Prakash <saiprakash8376@gmail.com>
2023-02-24 09:22:41 +0530

    MIC5: Circuit Breaker implementation using Resilience4j

--- UserService/src/main/java/com/micro/user/service/controllers/UserServiceController.java
+++ UserService/src/main/java/com/micro/user/service/controllers/UserServiceController.java
@@ -2,6 +2,9 @@ package com.micro.user.service.controllers;
 
 import com.micro.user.service.entities.User;
 import com.micro.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserServiceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceController.class);
 
     @Autowired
     private UserService userService;
@@ -28,8 +33,24 @@ public class UserServiceController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "userRatingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUserById(@PathVariable String userId) {
         User user1 = userService.getUserById(userId);
         return ResponseEntity.ok(user1);
+    }
+
+    /**
+     *
+     * Fallback method for circuit breaker
+     */
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception exception) {
+        logger.info("Executing Fallback method as one of the service seems to be down " + exception.getMessage());
+        User user = User.builder()
+                .age(12)
+                .userId("1234")
+                .userName("Dummy User")
+                .emailAddress("Dummy@info.com")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
